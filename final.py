0ebbbb8f5bbe332e4cbbd64660329a824303c1c8
mamaninang <63132161+mamaninang@users.noreply.github.com>
2020-04-17 23:01:02 -0700

    Update final.py

--- final.py
+++ final.py
@@ -10,27 +10,21 @@ from mininet.node import RemoteController
 class final_topo(Topo):
   def build(self):
     
-    # Examples!
-    # Create a host with a default route of the ethernet interface. You'll need to set the
-    # default gateway like this for every host you make on this assignment to make sure all 
-    # packets are sent out that port. Make sure to change the h# in the defaultRoute area
-    # and the MAC address when you add more hosts!
+    # Create hosts with a default route of the ethernet interface
     h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='10.0.1.101/24', defaultRoute="h1-eth0")
     h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='10.0.2.102/24', defaultRoute="h2-eth0")
     h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='10.0.3.103/24', defaultRoute="h3-eth0")
     h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='128.114.50.10/24', defaultRoute="h4-eth0")
     h5 = self.addHost('h5',mac='00:00:00:00:00:05',ip='10.0.4.104/24', defaultRoute="h5-eth0")
 
-    # Create a switch. No changes here from Lab 1.
-    s1 = self.addSwitch('s1') #floor 1 switch
-    s2 = self.addSwitch('s2') #floor 2 switch
-    s3 = self.addSwitch('s3') #floor 3 switch
-    s4 = self.addSwitch('s4') #core switch
-    s5 = self.addSwitch('s5') #data center switch
+    # Create the switches
+    s1 = self.addSwitch('s1')   #floor 1 switch
+    s2 = self.addSwitch('s2')   #floor 2 switch
+    s3 = self.addSwitch('s3')   #floor 3 switch
+    s4 = self.addSwitch('s4')   #core switch
+    s5 = self.addSwitch('s5')   #data center switch
 
-    # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on 
-    # Host 2. This is representing the physical port on the switch or host that you are 
-    # connecting to.
+    # Create the links that will connect a host to a switch, or connect a switch to another switch
     self.addLink(s1,h1, port1=1, port2=0)#switch 1 port 1 linked to host 1 port 0
     self.addLink(s2,h2, port1=1, port2=0)#switch 2 port 1 linked to host 2 port 0
     self.addLink(s3,h3, port1=1, port2=0)#switch 3 port 1 linked to host 3 port 0
